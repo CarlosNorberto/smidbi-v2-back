5e87dad9b8d6e783abe47004d2f1522565e24150
Carlos López Fernández <carlosnorbertolf@gmail.com>
2026-05-03 22:53:47 -0400

    install node-fetch

--- server/assistant_ai/services/openai.service.js
+++ server/assistant_ai/services/openai.service.js
@@ -1,7 +1,9 @@
+const fetch = require('node-fetch')
 const OpenAI = require('openai');
 
 const openai = new OpenAI({
-  apiKey: process.env.OPENAI_API_KEY
+  apiKey: process.env.OPENAI_API_KEY,
+  fetch: fetch
 });
 
 module.exports = openai;
